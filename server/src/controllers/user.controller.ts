8c489e24b0f14cd5d3ac50a48dec50a5fbdade14
nosaemma21 <emmanuelnosakhare052@gmail.com>
2026-02-08 17:53:46 +0100

    Update: Added controllers to the user.controller.ts file, updated user.routes.ts to include newly added controllers and added a githubid column to the prisma User schema to accomodate github sing ins

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,10 +1,59 @@
-import "express-async-handler";
+import handleAsync from "express-async-handler";
+import type { Request, Response, NextFunction } from "express";
 
 //----//
 import prisma from "../utils/prismaClient.js";
-import {} from "../errors/derivedErrors.js";
 import logger from "../utils/logger.js";
+import type {
+  RegisterInput,
+  LoginInput,
+} from "../validators/user.validators.js";
+import { registerInput, loginInput } from "../validators/user.validators.js";
 
 //controllers go here...
+const register = handleAsync(
+  (req: Request, res: Response, next: NextFunction) => {
+    // check if user exists from provided input
+    // hash password
+    // create user object using the vaildator
+    // store token object in cookie
+    // send token to client
+  },
+);
 
-export {};
+const logIn = handleAsync(() => {
+  // check if user exists from provided input
+  // bcrypt compare passwords
+  // if passwords match send token to client
+  // if not bounce the animal out
+  // store token in cookie
+  // send token to client
+});
+
+const logOut = handleAsync(() => {
+  // remove token from cookie
+  // redirect
+});
+
+const changePassword = handleAsync(() => {
+  // check if user is authenticated
+  // change password via db update
+  // can send a status message
+});
+
+const changeUsername = handleAsync(() => {
+  // check if user is authenticated
+  // change username via db update
+  // can send a status message
+});
+
+const githubSignin = handleAsync(() => {});
+
+export {
+  register,
+  logIn,
+  logOut,
+  changePassword,
+  changeUsername,
+  githubSignin,
+};
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,8 +1,23 @@
 import { Router } from "express";
 //importing controllers...
+import {
+  register,
+  logIn,
+  logOut,
+  changeUsername,
+  changePassword,
+  //   githubSignin,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.route("/");
+router.post("/register", register);
+router.post("/login", logIn);
+router.post("/logout", logOut);
+
+router.patch("/change-username", changeUsername); //will add the auth middleware inbetween
+router.patch("/change-password", changePassword); //will add the auth middlware inbetween
+
+//deal with gh soon
 
 export default router;
